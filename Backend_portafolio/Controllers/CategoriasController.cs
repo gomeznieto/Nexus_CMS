19b14541a7c7bdfece3d59da6f8ed3058b77a1d3
Alejandro Martín Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2024-02-20 14:52:50 -0300

    Categorias

--- Backend_portafolio/Controllers/CategoriasController.cs
+++ Backend_portafolio/Controllers/CategoriasController.cs
@@ -1,8 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Backend_portafolio.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Backend_portafolio.Controllers
 {
 	public class CategoriasController : Controller
 	{
+		[HttpGet]
+		public IActionResult Crear()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Crear(Categoria categoria)
+		{
+			if(!ModelState.IsValid)
+			{
+				return View(categoria);
+			}
+
+			return View();
+		}
 	}
 }
---
+++ Backend_portafolio/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend_portafolio.Models
+{
+	public class Categoria
+	{
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [Display(Name = "Categoria")]
+        [StringLength(maximumLength: 50, MinimumLength = 2, ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}")]
+        public string name { get; set; }
+    }
+}
