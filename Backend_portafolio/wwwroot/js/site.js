d8f4dd9c5b805a52efae75aa05dccd9de1917ad5
Alejandro Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2024-03-17 11:43:51 -0300

    Text format en Post

--- Backend_portafolio/wwwroot/js/site.js
+++ Backend_portafolio/wwwroot/js/site.js
@@ -250,3 +250,49 @@ async function borrador() {
     }
 
 }
+
+/* POST */
+
+function addTextFormat(style) {
+    let txtarea = document.getElementById("post_text");
+    const startPos = txtarea.selectionStart;
+    const endPos = txtarea.selectionEnd;
+    const before = txtarea.value.substring(0, startPos);
+    const after = txtarea.value.substring(startPos, txtarea.value.length);
+    var selectedText = txtarea.value.substring(startPos, endPos);
+    let styleFormat = textFormat(style, selectedText);
+
+    txtarea.value = before + `${styleFormat} ` + after.substring(selectedText.length);
+    txtarea.selectionStart = txtarea.selectionEnd = startPos + styleFormat.length;
+    txtarea.focus(); 
+}
+
+function textFormat(format, text) {
+
+    switch (format) {
+        case "h1":
+            return `# ${text}`;
+            break;
+        case "h2":
+            return `## ${text}`;
+            break;
+        case "h3":
+            return `### ${text}`;
+            break;
+        case "bold":
+            return `**${text}**`;
+            break;
+        case "italic":
+            return `*${text}*`;
+            break;
+        case "link":
+            return `[title](${text})`;
+            break;
+        case "image":
+            return `![alt text](${text})`;
+            break;
+        default:
+            return "?";
+            break;
+    }
+}
