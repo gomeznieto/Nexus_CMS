5511ffb1ffde5400b5d98a9e67946287bd7bdc52
Alejandro Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2024-12-30 11:18:05 -0300

    Corrección en tamaño de imágenes y extensión posibles

--- Backend_portafolio/Sevices/ImageService.cs
+++ Backend_portafolio/Sevices/ImageService.cs
@@ -8,23 +8,46 @@
     {
         public async Task<string> UploadImageAsync(IFormFile imageFile, string userName, string subfolder)
         {
+            // Carpeta raíz
+            string root = "img";
+
+            // Verifica que el archivo no sea nulo o vacío
             if (imageFile == null || imageFile.Length == 0)
             {
                 throw new ArgumentException("El archivo no es válido.");
             }
 
-            string uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "img", subfolder);
+            // Define la carpeta de subcarpetas y crea si no existe
+            string uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", root, subfolder);
             Directory.CreateDirectory(uploadsFolder);
 
-            string uniqueFileName = $"{Path.GetFileNameWithoutExtension(userName)}_{Path.GetRandomFileName()}.jpg";
+            // Verifica la extensión del archivo
+            string fileNameWithExtension = imageFile.FileName;
+            string extension = Path.GetExtension(fileNameWithExtension).ToLower();
+
+            if (extension != ".jpg" && extension != ".png")
+            {
+                throw new ArgumentException("El tipo de archivo no es válido. Solo se permiten .jpg y .png.");
+            }
+
+            // Define el nombre único del archivo
+            string uniqueFileName = $"{Path.GetFileNameWithoutExtension(userName)}_{Path.GetRandomFileName()}{extension}";
             string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+            // Verifica el tamaño del archivo (por ejemplo, 2MB)
+            const int maxFileSizeInBytes = 1 * 1024 * 1024; // 2 MB
+
+            if (imageFile.Length > maxFileSizeInBytes)
+            {
+                throw new ArgumentException($"No pudo guardarse la imagen.\nEl archivo excede el límite de tamaño permitido (1MB).");
+            }
 
             using (var stream = new FileStream(filePath, FileMode.Create))
             {
                 await imageFile.CopyToAsync(stream);
             }
 
-            return $"/img/{subfolder}/{uniqueFileName}";
+            return $"/{root}/{subfolder}/{uniqueFileName}";
         }
     }
 }
